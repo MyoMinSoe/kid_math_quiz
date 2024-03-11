3b2feefb856f290c3c1404f4bdd8b58eef9bf6ed
MyoMinSoe <myominsoe.min@gmail.com>
2024-03-11 16:09:21 +0630

    add myanmar font number my_extension function

---
+++ lib/kid_math_quizz/my_extension/font_converter.dart
@@ -0,0 +1,12 @@
+extension mm on String {
+  String mmNumberfont() => replaceAll('0', '၀')
+      .replaceAll('1', '၁')
+      .replaceAll('2', '၂')
+      .replaceAll('3', '၃')
+      .replaceAll('4', '၄')
+      .replaceAll('5', '၅')
+      .replaceAll('6', '၆')
+      .replaceAll('7', '၇')
+      .replaceAll('8', '၈')
+      .replaceAll('9', '၉');
+}
